6020747f2331387b3d3528ba8a634b6974855bec
CarlWus <1273070545@qq.com>
2020-06-28 20:36:11 +0800

    6 commit

---
+++ src/cn/anygloble/clone/TestCommit.java
@@ -0,0 +1,11 @@
+package cn.anygloble.clone;
+
+/**
+ * @author CarlWus
+ * @date 2020-06-28 - 20:34
+ */
+public class TestCommit {
+    public static void main(String[] args) {
+        System.out.println("ssssssssssss");
+    }
+}
